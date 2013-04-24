7fbfbb0d45dd3bfaa01d51c9f26885968d681951
kristenmills <kristen@kristen-mills.com>
2013-04-24 11:26:34 -0400

    updated discover links to print out all the links on every page

--- discovery.rb
+++ discovery.rb
@@ -4,19 +4,23 @@ require 'anemone'
 
 def crawl_domain (domain)
 	titles = []
-	Anemone.crawl("http://www.example.com/") do |anemone|
+	Anemone.crawl('http://localhost:8080/bodgeit') do |anemone|
   		anemone.on_every_page { |page| titles.push page.url rescue nil }
 	end
 
 	titles
 end
 
-def discover_links page
-  doc = Nokogiri::HTML(open(page))
-  doc.css('a').each do |link|
-    puts "Link discovered: #{link.content} @URL= #{link.attr('href')}"
+def discover_links(domain)
+  Anemone.crawl(domain) do |anemone|
+    anemone.on_every_page do |page| 
+      puts "Discovered on #{page.url}"
+      page.links.each do |x|
+        puts x
+      end
+      puts
+    end
   end
 end
 
 discover_links 'http://localhost:8080/bodgeit'
-print crawl_domain("google.com")
