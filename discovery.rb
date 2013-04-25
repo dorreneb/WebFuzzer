32a1ddc6534738b207b8f876d8270d5a83d16141
kristenmills <kristen@kristen-mills.com>
2013-04-25 14:16:58 -0400

    Page guessing working I think

--- discovery.rb
+++ discovery.rb
@@ -11,16 +11,30 @@ def crawl_domain (domain)
 	titles
 end
 
-def discover_links(domain)
+def discover_links(domain, common_url_path)
+  pages = read_page_guesses(common_url_path)
   Anemone.crawl(domain) do |anemone|
-    anemone.on_every_page do |page| 
+    anemone.on_every_page do |page|
       puts "Discovered on #{page.url}"
       page.links.each do |x|
         puts x
+        pages[x.path] = true
       end
       puts
     end
   end
+  puts "Pages not linked"
+  pages.each do |k, v|
+    puts k if !v
+  end
 end
 
-discover_links 'http://localhost:8080/bodgeit'
+def read_page_guesses(common_url_path)
+  hash = Hash.new
+  open(common_url_path).each do |line|
+    hash[line.chomp] = false
+  end
+  hash
+end
+
+discover_links 'http://localhost:8080/bodgeit', 'commonurls-bodgeit.txt'
