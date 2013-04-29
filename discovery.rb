dad01db17493230bdf0c1e571f7703c2763e8add
Dorrene Brown <dorreneb@gmail.com>
2013-04-28 23:34:31 -0400

    Checking in current code to test out mechanize

--- discovery.rb
+++ discovery.rb
@@ -1,12 +1,70 @@
 require 'Anemone'
+require 'set'
+require 'open-uri'
 
-def crawl_domain (domain)
-	titles = []
-	Anemone.crawl("http://www.example.com/") do |anemone|
-  		anemone.on_every_page { |page| titles.push page.url rescue nil }
+class Crawler
+
+	def discover_pages(domain)
+		titles = Set.new
+
+		Anemone.crawl(domain) do |anemone|
+	  		anemone.on_every_page do |page|
+	  			url = page.url
+	  			titles.add ("#{url.host}:#{url.port}#{url.path}") if not page.doc.nil?
+  			end
+		end
+
+		titles
 	end
 
-	titles
-end
+	def discover_inputs(page_url = "http://127.0.0.1")
+		inputs = []
+ 
+		puts "Searching #{page_url}..."
+		page = Nokogiri::HTML(open(page_url)) 
+		page.xpath("//input").each do |input|
+			inputs.add input
+		end
+		
+		inputs
+	end
 
-print crawl_domain("google.com")
+	#
+	# I don't reuse code from discover_pages and discover_inputs here because it is fairly 
+	# expensive to open nokogiri pages. Utilizing the pages that are already open when 
+	# Anemone is crawling makes it faster when fuzzing large websites. Consider discover_pages 
+	# and discover_inputs learning exercises while getting to discover_all_inputs.
+	#
+	def discover_all_inputs(root_url = "http://127.0.0.1")
+		visited_pages = Set.new
+
+		puts "Searching for inputs from root #{root_url}..."
+		Anemone.crawl(root_url) do |anemone|
+	  		anemone.on_every_page do |page|
+	  			url = "#{page.url.host}:#{page.url.port}#{page.url.path}"
+
+	  			# only search on pages that have not yet been fuzzed
+	  			if not visited_pages.include? url
+	  				# search page
+		  			puts "\tSearching #{url}..."
+		  			if not page.doc.nil?
+		  				# print inputs
+		  				inputs = page.doc.xpath("//input")
+		  				puts "\t\tInputs:" if inputs.size > 0
+		  				inputs.each { |input| puts "\t\t\t#{input}" }
+		  				
+		  				# print cookies (if exist)
+		  				if page.cookies.size > 0
+		  					puts "\t\tCookies:"
+		  					page.cookies.each { |cookie| puts "\t\t\t#{cookie}"}
+		  				end
+		  			end
+
+		  			#mark page as visited
+		  			visited_pages.add url
+		  		end
+  			end
+		end
+	end
+
+end
--- testbench.rb
+++ testbench.rb
@@ -1,14 +1,47 @@
 #!/usr/bin/env ruby
 
+require './discovery.rb'
+
 # A super simple test bench to execute features of the fuzzer.
-# Add features by adding ["Prompt text", proc{method with code to call}] 
+# Add features by adding ["index) prompt text", proc{method with code to call}] 
 # to the options array.
 
 def crawl
-	puts "hello"
+	puts "Enter URL root (Leave blank for 127.0.0.1):"
+	url = gets.chomp
+	url = "http://127.0.0.1" if url.nil? or url.empty?
+
+	crawl = Crawler.new
+	pages = crawl.discover_pages(url)
+
+	pages.each { |url|	puts url } if not pages.nil?
 end
 
-options = [["1) Crawl Webpage", proc{ crawl }], ["2) Quit", proc{ Process.exit }]]
+def get_inputs
+	puts "What URL do you want to get inputs from? (Leave blank to use results of crawl): " 
+	url = gets.chomp
+
+	crawl = Crawler.new
+	inputs = crawl.discover_inputs(url)
+
+	inputs.each { |input| puts input } if not inputs.nil?
+
+end
+
+def get_all_inputs
+	puts "Enter URL root (Leave blank for 127.0.0.1):"
+	url = gets.chomp
+	url = "http://127.0.0.1" if url.nil? or url.empty?
+
+	crawl = Crawler.new
+	pages = crawl.discover_all_inputs(url)
+end
+
+options = [
+	["1) Crawl Webpage", proc{ crawl }], 
+	["2) Get Inputs from Page", proc{ get_inputs }], 
+	["3) Get Inputs from Crawl", proc{ get_all_inputs }],
+	["4) Quit", proc{ Process.exit }]]
 
 while true
 	puts "Pick an Option: "
